7e4d55fdb37480aceda34df46557aec81bfd0809
fianfebry <fian.febry@sprout.co.id>
2023-10-27 13:06:49 +0700

    bugfix button enter jotion

--- app/(marketing)/_components/Heading.tsx
+++ app/(marketing)/_components/Heading.tsx
@@ -1,15 +1,25 @@
 "use client";
+import { Spinner } from "@/components/spinner";
 import { Button } from "@/components/ui/button";
+import { SignInButton } from "@clerk/clerk-react";
+import { useConvexAuth } from "convex/react";
 import { ArrowRight } from "lucide-react";
+import { useRouter } from "next/navigation";
 import React from "react";
 
 const Heading = () => {
+  const navigate = useRouter();
+  const { isAuthenticated, isLoading } = useConvexAuth();
+
   const [buttonOnHoover, setButtonOnHoover] = React.useState(false);
   const handleButtonHover = () => {
     setButtonOnHoover(true);
   };
   const handleButtonLeaveHover = () => {
     setButtonOnHoover(false);
+  };
+  const handleClick = () => {
+    navigate.push("/documents");
   };
   return (
     <div className="max-w-3xl space-y-4">
@@ -21,18 +31,39 @@ const Heading = () => {
         Jotion is the connected workspace where <br />
         better, faster work happens.
       </h3>
-      <Button
-        className="relative"
-        onMouseEnter={handleButtonHover}
-        onMouseLeave={handleButtonLeaveHover}
-      >
-        Enter Jotion
-        <ArrowRight
-          className={`w-4 h-4 ml-2 ${
-            buttonOnHoover ? "transform translate-x-2" : ""
-          }`}
-        />
-      </Button>
+      {isLoading && <Spinner />}
+      {!isAuthenticated && !isLoading && (
+        <SignInButton mode="modal">
+          <Button
+            className="relative"
+            onMouseEnter={handleButtonHover}
+            onMouseLeave={handleButtonLeaveHover}
+            onClick={handleClick}
+          >
+            Enter Jotion
+            <ArrowRight
+              className={`w-4 h-4 ml-2 ${
+                buttonOnHoover ? "transform translate-x-2" : ""
+              }`}
+            />
+          </Button>
+        </SignInButton>
+      )}
+      {isAuthenticated && !isLoading && (
+        <Button
+          className="relative"
+          onMouseEnter={handleButtonHover}
+          onMouseLeave={handleButtonLeaveHover}
+          onClick={handleClick}
+        >
+          Enter Jotion
+          <ArrowRight
+            className={`w-4 h-4 ml-2 ${
+              buttonOnHoover ? "transform translate-x-2" : ""
+            }`}
+          />
+        </Button>
+      )}
     </div>
   );
 };
